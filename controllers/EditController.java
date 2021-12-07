1d82a882bef7c767706dec591d4a17df9d359f61
cbyce <cbyce@uwo.ca>
2021-12-07 16:41:21 -0500

    Added comments to controllers

--- controllers/EditController.java
+++ controllers/EditController.java
@@ -4,26 +4,30 @@ import java.util.Date;
 import models.*;
 
 public class EditController {
-    Page model;
-    views.EditingView view;
+    Page model;  // The page model
+    views.EditingView view; // The editor view
 
+    // Constructor for edit controller, assigns the args model and view to this model and this view
     public EditController(Page model, views.EditingView view) {
         this.model = model;
         this.view = view;
     }
 
+    // Method to add multimedia
     public void addMultimedia(String url, String author, Date date) {
-        Multimedia m = new Multimedia(url, author, date);
-        model.addMedia(m);
+        Multimedia m = new Multimedia(url, author, date); // Constructs a new multimedia model
+        model.addMedia(m); // Adds multimedia to the page model
     }
 
+    // Method to add text
     public void addText(String text, String author, Date date) {
-        Text t = new Text(text, author, date);
-        model.addText(t);
+        Text t = new Text(text, author, date); // Constructs a new text model
+        model.addText(t); // Adds text to the page model
     }
 
+    // Method to add a comment
     public void addComment(String text, String author, Date date) {
-        Comment c = new Comment(text, author, date);
-        model.addComment(c);
+        Comment c = new Comment(text, author, date); // Constructs a new comment model
+        model.addComment(c); // Adds comment to the page model
     }
 }
--- controllers/ViewController.java
+++ controllers/ViewController.java
@@ -4,16 +4,18 @@ import java.util.Date;
 import models.*;
 
 public class ViewController {
-    models.Page model;
-    views.ViewingView view;
+    models.Page model; // The page model
+    views.ViewingView view; // The veiw only view
 
+    // Constructor for view controller, assigns the args model and view to this model and this view
     public ViewController(models.Page model, views.ViewingView view) {
         this.model = model;
         this.view = view;
     }
 
+    // Add comment functionality for the view only 
     public void addComment(String text, String author, Date date) {
-        Comment c = new Comment(text, author, date);
-        model.addComment(c);
+        Comment c = new Comment(text, author, date); // Constructs a new comment model
+        model.addComment(c); // Adds comment to the page model
     }
 }
