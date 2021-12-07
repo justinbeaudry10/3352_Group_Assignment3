80addac21cb61ef17fd5d97006a6325227cd2e70
Roshan Nanthapalan <rnanthap@uwo.ca>
2021-12-07 15:57:36 -0500

    Added edit methods to Page

--- models/Page.java
+++ models/Page.java
@@ -28,6 +28,24 @@ public class Page {
         texts.add(text);
     }
 
+    public void editComment(Comment comment, String text)
+    {
+        int index = comments.indexOf(comment);
+        comments.get(index).setCommentString(text);
+    }
+
+    public void editMedia(Multimedia media, String url)
+    {
+        int index = medias.indexOf(media);
+        medias.get(index).setUrl(url);
+    }
+
+    public void editText(Text text, String textStr)
+    {
+        int index = texts.indexOf(text);
+        texts.get(index).setTextString(textStr);
+    }
+
 
 
 
