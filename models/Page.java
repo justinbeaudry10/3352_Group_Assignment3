2bb21ca58f94e06dde40fc7f5964b9358ec0c71c
Roshan Nanthapalan <rnanthap@uwo.ca>
2021-12-07 15:35:47 -0500

    Page

--- models/Page.java
+++ models/Page.java
@@ -1,5 +1,35 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Page {
+
+    ArrayList<Comment> comments = new ArrayList<>();
+    ArrayList<Multimedia> medias = new ArrayList<>();
+    ArrayList<Text> texts = new ArrayList<>();
+
+    public Page()
+    {
+
+    }
+
+    public void addComment(Comment comment)
+    {
+        comments.add(comment);
+    }
+
+    public void addMedia(Multimedia multimedia)
+    {
+        medias.add(multimedia);
+    }
+
+    public void addText(Text text)
+    {
+        texts.add(text);
+    }
+
+
+
+
 
 }
