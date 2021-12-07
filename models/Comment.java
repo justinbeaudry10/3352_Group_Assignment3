f1dcbaacf546177b5c241d7d20d61524915a49c6
bmendoza23 <bmendoz2@uwo.ca>
2021-12-07 15:23:52 -0500

    Comment constructor

--- models/Comment.java
+++ models/Comment.java
@@ -1,5 +1,17 @@
 package models;
+import java.util.Date;
 
 public class Comment {
-    
+    //Variable declaration
+    String commentText;
+    String author;
+    Date date;
+
+    //Comment constructor
+    public Comment(String commentText, String author, Date date)
+    {
+        this.commentText = commentText;
+        this.author = author;
+        this.date = date;
+    }
 }
