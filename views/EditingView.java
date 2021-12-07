11bed230a44fbf2be5c955e03d15c095fd6370a3
justinbeaudry10 <jbeaudr5@uwo.ca>
2021-12-07 16:06:01 -0500

    Updated EditingView

--- views/EditingView.java
+++ views/EditingView.java
@@ -1,5 +1,22 @@
 package views;
 
+import java.util.Date;
+
 public class EditingView {
 
+    public void displayEditBtns() {
+        // Displays all the buttons that allow the owner to edit the page
+    }
+
+    public void showComment(String commentTxt, String author, Date date) {
+        // Displays a comment
+    }
+
+    public void showText(String text, String author, Date date) {
+        // Display text
+    }
+
+    public void showMultiedia(String url, String author, Date date) {
+        // Display multimedia
+    }
 }
