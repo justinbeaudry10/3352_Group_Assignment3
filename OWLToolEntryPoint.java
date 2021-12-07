bf9907822651d425177147a41da46b1b8dee7420
justinbeaudry10 <jbeaudr5@uwo.ca>
2021-12-07 15:23:28 -0500

    Updated Entry Point

--- OWLToolEntryPoint.java
+++ OWLToolEntryPoint.java
@@ -1,5 +1,11 @@
 public class OWLToolEntryPoint {
     public static void main(String[] args) {
+        models.Page m = new models.Page();
+        views.EditingView editV = new views.EditingView();
+        views.ViewingView viewV = new views.ViewingView();
+
+        controllers.EditController editC = new controllers.EditController(m, editV);
+        controllers.ViewController viewC = new controllers.ViewController(m, viewV);
 
     }
 }
