2aed5d6743eeeebe3254b88c6db1afafd0683cdd
cbyce <cbyce@uwo.ca>
2021-12-07 15:59:49 -0500

    Viewing view

--- views/ViewingView.java
+++ views/ViewingView.java
@@ -1,5 +1,18 @@
 package views;
+import java.util.Date;
 
 public class ViewingView {
+
+    public void showComment(String commentTxt, String author, Date date) {
+        // Displays a comment
+    }
+
+    public void showText(String text, String author, Date date) {
+        // Display text
+    }
+
+    public void showMultiedia(String url, String author, Date date) {
+        // Display multimedia
+    }
 
 }
