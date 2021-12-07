e3de7c05d1f9f12cf175f9c9a8e1cee84f18c539
justinbeaudry10 <jbeaudr5@uwo.ca>
2021-12-07 16:35:26 -0500

    Commented main method

--- OWLToolEntryPoint.java
+++ OWLToolEntryPoint.java
@@ -2,10 +2,12 @@ import views.*;
 
 public class OWLToolEntryPoint {
     public static void main(String[] args) {
+        // Creates new Page and views
         models.Page m = new models.Page();
         EditingView editV = new EditingView();
         ViewingView viewV = new ViewingView();
 
+        // Creates new controllers and attaches the appropriate view
         controllers.EditController editC = new controllers.EditController(m, editV);
         controllers.ViewController viewC = new controllers.ViewController(m, viewV);
 
